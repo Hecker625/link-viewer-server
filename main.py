d8d5fcc407c76a29dee304e8e80bc5e873a7bb96
Hecker625 <kyler.shah@gmail.com>
2026-02-21 19:59:55 -0800

    Add a simple Flask proxy server

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def proxy():
+    url = request.args.get("url")
+
+    if not url:
+        return "Add this to the end of the url: ?url=https://your-website.com"
+
+    r = requests.get(url)
+
+    return Response(
+        r.content,
+        status=r.status_code,
+        content_type=r.headers.get("Content-Type")
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
